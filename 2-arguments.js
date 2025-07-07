a943f2e0b7151d16ec44bb4dbee757d789d8a9e2
samuel-udoh <chukwuemekasam16@gmail.com>
2025-07-07 15:55:06 +0100

    fix 2

--- 2-arguments.js
+++ 2-arguments.js
@@ -3,6 +3,9 @@ let len_argv = process.argv.length
 
 if (len_argv <= 2) {
     console.log("No argument");
+} else if (len_argv == 3) {
+    console.log("Argument found")
 } else {
-    console.log("Arguments found")
+    console.log("Arguments found");
+
 }
